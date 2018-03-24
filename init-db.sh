eac84f231c961cf582ef883fb5b8ec29fe37944c
tomastrajan <tomas.trajan@gmail.com>
2018-03-24 16:50:52 +1300

    feat(data): add init db script

---
+++ init-db.sh
@@ -0,0 +1,10 @@
+node . init --name "Vue" --repo "vuejs/vue" --url https://github.com/vuejs/vue/releases --urlType github --hashtags vue,vuejs
+node . init --name "Angular" --repo angular/angular --url https://github.com/angular/angular/blob/master/CHANGELOG.md --urlType changelog --hashtags angular,typescript
+node . init --name "Angular CLI" --repo angular/angular-cli --url https://github.com/angular/angular-cli/releases --urlType github --hashtags angular,cli,angularcli,tooling
+node . init --name "RxJS" --repo "ReactiveX/rxjs" --urlType changelog --url https://github.com/ReactiveX/rxjs/blob/master/CHANGELOG.md --hashtags rxjs,observable,reactive
+node . init --name "Typescript" --repo "Microsoft/TypeScript" --urlType github --url https://github.com/Microsoft/TypeScript/releases --hashtags typescript,ts,types
+node . init --name "Webpack" --repo "webpack/webpack" --urlType github --url https://github.com/webpack/webpack/releases --hashtags webpack,bundler,tooling
+node . init --name "React" --repo "facebook/react" --urlType github --url https://github.com/facebook/react/releases --hashtags react,reactjs
+node . init --name "Ember.js" --repo "emberjs/ember.js" --urlType github --url https://github.com/emberjs/ember.js/releases --hashtags ember,emberjs
+node . init --name "Create React App" --repo "facebook/create-react-app" --urlType github --url https://github.com/facebook/create-react-app/releases --hashtags cra,react,tooling
+node . init --name "Prettier" --repo "prettier/prettier" --urlType github --url https://github.com/prettier/prettier/releases --hashtags prettier,codestyle,tooling
