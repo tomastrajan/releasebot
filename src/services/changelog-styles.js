03a9c53dadc55091205bb493aa14a5197944d796
tomastrajan <tomas.trajan@gmail.com>
2018-03-27 00:41:48 +1300

    feat(changelog): use puppeteer instead of webshot

--- src/services/changelog-styles.js
+++ src/services/changelog-styles.js
@@ -5,44 +5,35 @@ const COLOR_TEXT_LINK = '#ffde24';
 const COLOR_TEXT_HIGHLIGHT = '#ffffff';
 const COLOR_TEXT_HIGHLIGHT_DARKER = '#c4932d';
 
-export const githubReleaseStyles = `
-  .release-meta { display: none; } 
-  .release-body { 
+export const getChangelogStyles = selector => `
+  .file {
+    border: 0 !important;
+  }
+  ${selector} { 
     width: 93% !important;
     border: 0 !important; 
-    margin: 40px 30px; 
+    margin: 40px 40px 70px 40px; 
     box-shadow: rgba(0, 0, 0, 0.55) 0px 20px 68px; 
-    border-radius: 5px; 
-    background-color: ${COLOR_BACKGROUND};
+    border-radius: 5px  !important; 
+    background-color: ${COLOR_BACKGROUND} !important;
     color: ${COLOR_TEXT};
   }
-  .release-body h1 a {
+  ${selector} h1 a {
     color: ${COLOR_TEXT_HIGHLIGHT_DARKER};
   }
-  .release-body li strong, 
-  .release-body h2, 
-  .release-body h3, 
-  .release-body h4 {
+  ${selector} li strong, 
+  ${selector} h2, 
+  ${selector} h3, 
+  ${selector} h4 {
     color: ${COLOR_TEXT_HIGHLIGHT};
   }
-  .release-body li a, .release-header p a {
+  ${selector} li a, .release-header p a {
     color: ${COLOR_TEXT_LINK};
   }
-  .release-body code {
+  ${selector} code {
     background-color: ${COLOR_BACKGROUND_CODE} !important;
   }
-  .release-body pre {
+  ${selector} pre {
     background-color: ${COLOR_BACKGROUND_CODE} !important;
   }
 `;
-
-export const changelogReleaseStyles = '.markdown-body h1 { padding-top: 1000px }';
-
-export const changelogReleaseOffsetOption = {
-  shotOffset: {
-    left: 40,
-    right: 40,
-    top: 1680,
-    bottom: 0
-  }
-};
--- src/services/changelog.js
+++ src/services/changelog.js
@@ -1,72 +1,87 @@
+import fs from 'fs';
 import { getLogger } from 'log4js';
-import webshot from 'webshot';
+import puppeteer from 'puppeteer';
 
-import { getCommitDate, getRepoTags } from '../api/github';
-
-import {
-  changelogReleaseOffsetOption,
-  changelogReleaseStyles,
-  githubReleaseStyles
-} from './changelog-styles';
+import { getChangelogStyles } from './changelog-styles';
 import { getChangelogFileUrl, getChangelogReleaseUrl } from './url';
 
 const logger = getLogger('Changelog Service');
 
-export const getChangelogAsImage = async (project, version, asFile) =>
-  await (project.type === 'github'
-    ? getChangelogFromGithubRelease(project, version, asFile)
-    : getChangelogFromChangelogMd(project, version, asFile));
-
-const getChangelogFromChangelogMd = async (project, version, asFile) =>
-  getUrlAsImageBuffer(
-    getChangelogFileUrl(project.repo),
-    {
-      renderDelay: 1000,
-      customCSS: changelogReleaseStyles,
-      ...changelogReleaseOffsetOption
-    },
-    asFile
-  );
-
-const getChangelogFromGithubRelease = (project, version, asFile) =>
-  getUrlAsImageBuffer(
-    getChangelogReleaseUrl(project.repo, version),
-    {
-      captureSelector: '.release',
-      customCSS: githubReleaseStyles
-    },
-    asFile
-  );
-
-const getUrlAsImageBuffer = (url, options, asFile) =>
-  new Promise((resolve, reject) => {
-    if (asFile) {
-      logger.info('Get url as image file', url, options);
-      webshot(url, 'test.png', options, err => {
-        err
-          ? logger.error('Get url as image file failed', err)
-          : logger.info('Get url as image file successful');
-        process.exit(0);
-      });
-      return;
+export const getChangelogAsImage = async (project, version, asFile) => {
+  try {
+    const { type, repo } = project;
+    logger.info('Get changelog as image for:', type, repo, version);
+    const isGithub = type === 'github';
+    const url = isGithub
+      ? getChangelogReleaseUrl(repo, version)
+      : getChangelogFileUrl(repo, version);
+    const selector = isGithub ? '.release-body' : '.markdown-body';
+    const page = await getPage(url, getChangelogStyles(selector));
+    if (!isGithub) {
+      logger.info('Get changelog as image remove other versions');
+      await removeIrrelevantVersions(page, selector, version);
     }
-    logger.info('Get url as image buffer', url, options);
-    const stream = webshot(url, { streamType: 'png', ...options });
-    const receivedDataChunks = [];
-    stream.on('error', err => {
-      logger.error('Get url as image buffer failed', err);
-      reject(err);
-    });
-    stream.on('data', data => receivedDataChunks.push(data));
-    stream.on('end', () => {
-      logger.info('Get url as image buffer finished');
-      resolve(Buffer.concat(receivedDataChunks));
-    });
-  });
+    logger.info('Get changelog as image start:', selector);
+    const screenShot = await getScreenShot(page, selector);
+    logger.info('Get changelog as image success:', type, repo, version);
+    return asFile ? saveToFileAndExit(screenShot, repo) : screenShot;
+  } catch (err) {
+    logger.error('Get changelog from github release failed', err);
+  }
+};
 
-const resolveChangelogReleaseAnchorLink = async repo => {
-  const tags = await getRepoTags(repo);
-  const sha = tags.filter(t => t.name === version)[0].commit.sha;
-  const date = await getCommitDate(repo, sha);
-  return `${version.replace(/\./gi, '')}-${date.split('T')[0]}`;
+const removeIrrelevantVersions = async (page, selector, version) =>
+  page.evaluate(
+    (selector, version) => {
+      const isVersionNode = node =>
+        ['h1', 'h2', 'h3'].includes(node.nodeName.toLowerCase()) &&
+        /v?\d\.\d\.\d.*/.test(node.innerText);
+      let isIrrelevantNode = true;
+      Array.from(document.querySelector(selector).childNodes)
+        .filter(node => {
+          if (!isIrrelevantNode && isVersionNode(node)) {
+            isIrrelevantNode = true;
+          }
+          if (isVersionNode(node) && node.innerText.includes(version)) {
+            isIrrelevantNode = false;
+          }
+          return isIrrelevantNode;
+        })
+        .forEach(node => node.remove());
+    },
+    selector,
+    version
+  );
+
+const getScreenShot = async (page, selector) => {
+  const { x, y, width, height } = await page.evaluate(selector => {
+    const element = document.querySelector(selector);
+    const { x, y, width, height } = element.getBoundingClientRect();
+    return { x, y, width, height };
+  }, selector);
+  return await page.screenshot({
+    type: 'png',
+    clip: { x: x - 40, y: y - 40, width: width + 80, height: height + 110 }
+  });
+};
+
+const getPage = async (url, styles) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url, { waitUntil: 'networkidle2' });
+  await page.setViewport({ width: 1024, height: 768, deviceScaleFactor: 2 });
+  await page.addStyleTag({ content: styles });
+  page.on('console', msg => logger.debug(msg.text()));
+  return page;
+};
+
+const saveToFileAndExit = (screenShot, repo) => {
+  const filename = `changelog_${repo.replace('/', '_')}.png`;
+  fs.writeFile(filename, screenShot, err => {
+    if (err) {
+      logger.error(err);
+    }
+    logger.info('Get changelog as image file saved:', filename);
+    process.exit(0);
+  });
 };
