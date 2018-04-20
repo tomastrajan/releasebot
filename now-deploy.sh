bce831b398a6f62476a8d5db47ea32c980c786fb
tomastrajan <tomas.trajan@gmail.com>
2018-04-19 14:47:45 -1000

    chore(deployment): adjust build script and config for new now cli version

--- now-deploy.sh
+++ now-deploy.sh
@@ -2,5 +2,5 @@
 now \
 && sleep 5 \
 && now alias \
-&& now scale releasebutler.now.sh 1 \
-&& echo y | now rm $(now ls | egrep "releasebot.*now\.sh" -o | tail -n1)
+&& now scale releasebutler.now.sh bru1 0 \
+&& echo y | now rm $(now ls releasebot | egrep "releasebot-.*now\.sh" -o | tail -n1)
