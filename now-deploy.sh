e06206929e6799e5a40ae65ccfa1fa556cb616f6
Tomas Trajan <tomas.trajan@gmail.com>
2018-07-21 11:42:04 +0700

    feat(deployment): adjust deployment scaling

--- now-deploy.sh
+++ now-deploy.sh
@@ -3,5 +3,5 @@ now \
 && sleep 5 \
 && now alias \
 && now scale releasebutler.now.sh bru1 0 \
-&& now scale releasebutler.now.sh sfo1 1 \
+&& now scale releasebutler.now.sh sfo1 1 1 \
 && echo y | now rm $(now ls releasebot | egrep "releasebot-.*now\.sh" -o | tail -n1)
