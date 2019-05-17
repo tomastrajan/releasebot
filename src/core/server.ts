14bfd6163b91d356d66b79a271e9bd5ddd903ef0
tomastrajan <tomas.trajan@gmail.com>
2019-05-17 23:41:11 +0200

    feat(server): add cors support

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,13 +1,28 @@
 import * as express from 'express';
+import * as cors from 'cors';
 import { getLogger } from 'log4js';
 
 import { getChangelogImage } from '../handlers/changelog';
 
 const logger = getLogger('Server');
 const PORT = 8080;
+const CORS_ALLOWED = [
+  'https://releasebutler.now.sh',
+  'https://releasebutler.stackblitz.io'
+]
 
 export function runServer() {
   const app = express();
+
+  app.use(cors({
+    origin(origin, next) {
+      if (!origin || CORS_ALLOWED.indexOf(origin) !== -1) {
+        next(null, true);
+      } else {
+        next(new Error('Not allowed by CORS'))
+      }
+    }
+  }))
 
   app.use(express.static('public'));
 
