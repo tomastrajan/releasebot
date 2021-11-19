b606412af3a65767491812c7cce4192c2cabb8cc
tomastrajan <tomas.trajan@gmail.com>
2021-11-19 12:58:07 +0100

    feat(db): adjust tracked projects

--- init-db.sh
+++ init-db.sh
@@ -1,7 +1,7 @@
 ts-node . init "Vue" "vuejs/vue" github vue,vuejs
 ts-node . init "Angular" angular/angular changelog angular,typescript
 ts-node . init "Angular CLI" angular/angular-cli github angular,cli,angularcli,tooling
-ts-node . init "Angular Material" angular/material2 changelog angular,material,components,cdk
+ts-node . init "Angular Components (Material)" angular/components changelog angular,material,components,cdk
 ts-node . init "RxJS" "ReactiveX/rxjs" changelog rxjs,observable,reactive
 ts-node . init "Typescript" "Microsoft/TypeScript" github typescript,ts,types
 ts-node . init "Webpack" "webpack/webpack" github webpack,bundler,tooling
@@ -11,12 +11,10 @@ ts-node . init "Create React App" "facebook/create-react-app" github cra,react,t
 ts-node . init "Prettier" "prettier/prettier" github prettier,codestyle,tooling
 ts-node . init "NPM" "npm/npm" github npm,node,packagemanager
 ts-node . init "nest" "nestjs/nest" github nest,nestjs,nodejs-framework,typescript
-ts-node . init "Next.js" "zeit/next.js" github nextjs,react,node,universal,ssr
 ts-node . init "Svelte" "sveltejs/svelte" changelog sveltejs,svelte,javascript,compiler
 ts-node . init "Redux" "reactjs/redux" github redux,react,reactjs,flow
 ts-node . init "MobX" "mobxjs/mobx" changelog mobx,reactive,javascript,statemanagement
 ts-node . init "Material UI" "mui-org/material-ui" github materialdesign,react,reactjs,components,javascript
-ts-node . init "Mocha.js" "mochajs/mocha" github mocha,mochajs,testing,tdd,javascript,nodejs
 ts-node . init "Puppeteer" "GoogleChrome/puppeteer" github puppeteer,headlesschrome,nodejs,ssr
 ts-node . init "GraphQL.js" "graphql/graphql-js" github graphql,graphqljs,api,javascript
 ts-node . init "Bootstrap" "twbs/bootstrap" github bootstrap,css,scss,cssframework,responsive,design,components
@@ -32,3 +30,6 @@ ts-node . init "NgRx" "ngrx/platform" changelog ngrx,angular,rxjs,reactive,store
 ts-node . init "xLayers" "xlayers/xlayers" changelog angular,design,developer,ui,ux,codegen,sketch,react,vuejs,litelement,stenciljs,webcomponents
 ts-node . init "nestjs-addons" "nestjs-addons/in-memory-db" github nest,nestjs,nodejs-framework,typescript
 ts-node . init "Angular Extensions Elements" "angular-extensions/elements" changelog angular,elements,webcomponents,stenciljs,litelement,lazyloading,microfrontends
+ts-node . init "Snowpack" "snowpackjs/snowpack" changelog bundler,tooling,esm,javascript,frontend
+ts-node . init "esbuild" "evanw/esbuild" changelog bundler,tooling,esm,javascript,frontend
+ts-node . init "Parcel" "parcel-bundler/parcel" changelog bundler,tooling,esm,javascript,frontend
